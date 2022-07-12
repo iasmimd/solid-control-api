0bd5f588a0670d9e238ae39afeec512091e3469f
iasmim <iasmimstuff@gmail.com>
2022-07-12 14:22:10 -0300

    feat: ticket service created

--- src/service/ticket.service.ts
+++ src/service/ticket.service.ts
@@ -1,5 +1,5 @@
-import { userInfo } from 'os';
 import { AppDataSource } from '../data-source';
+import { Ticket } from '../entities/ticket.entity';
 
 class TicketService {
   static async create(user_id: string) {
@@ -8,6 +8,18 @@ class TicketService {
     const findUser = await ticketRepository.findOne({
       where: { user_id },
     });
+
+    if (!findUser) {
+      throw new Error('User not found');
+    }
+
+    const ticket = new Ticket();
+    ticket.cart;
+
+    ticketRepository.create(ticket);
+    ticketRepository.save(ticket);
+
+    return ticket;
   }
 }
 

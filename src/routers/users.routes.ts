26320f1f1c43644727f2a21b91af7415407b93f9
Tarcila <tarcilasg@gmail.com>
2022-07-12 20:14:38 -0300

    feat@user:[add] crud in users.routes.ts

---
+++ src/routers/users.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import UsersControllers from "../controller/user.controller";
+
+const routes = Router();
+
+export const userRoutes = () => {
+    routes.post("/register", UsersControllers.createUserController);
+    routes.post("/login", UsersControllers.loginUserController);
+    routes.get("/users/:id", UsersControllers.listUserRetrieveController);
+    routes.patch("/users/:id", UsersControllers.updateUserController);
+    routes.delete("/users/:id", UsersControllers.deleteUserController);
+};

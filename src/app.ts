5a410e3cec0376f1263b750578145996686a0ff5
Tarcila <tarcilasg@gmail.com>
2022-07-12 20:12:15 -0300

    feat@user:[add] user routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import routes from './routers';
+import { userRoutes } from './routers/users.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+
+app.use(userRoutes);
 
 export default app

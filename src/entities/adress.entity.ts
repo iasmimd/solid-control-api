542f1f445bc5035b0021e431df4c2ee183996619
Lucas Golle <lucasgolle@pop-os.localdomain>
2022-07-12 13:51:04 -0300

    update: ManyToMany relationship create adress/entity.ts file

--- src/entities/adress.entity.ts
+++ src/entities/adress.entity.ts
@@ -1,5 +1,42 @@
-import { Entity } from "typeorm";
-import { v4 as uuid } from "uuid";
+import {
+    Entity,
+    Column,
+    PrimaryColumn,
+    ManyToMany,
+    JoinTable
+  } from "typeorm";
+  import { v4 as uuid } from "uuid";
+  
+  @Entity()
+  export class Adress {
+    @PrimaryColumn("uuid")
+    readonly id: string;
+  
+    @Column()
+    number: number;
+  
+    @Column("varchar", { length: 256, nullable: false})
+    street: string;
+  
+    @Column("varchar", { length: 256 })
+    complement: string;
+  
+    @Column("varchar", { length: 2, nullable: false})
+    state: string;
+  
+    @Column({ nullable: false })
+    zip_code: number;
+  
+    @Column("varchar", { length: 2, nullable: false})
+    country: string;
 
-@Entity()
-export class Adress {}
+    @ManyToMany(() => Users)
+    @JoinTable()
+    user_id: Users[]
+    
+    constructor() {
+      if ("this.id") {
+        this.id = uuid();
+      }
+    }
+  }

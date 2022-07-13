a5881606f57fbfb9885a5915ee3612e87c4fb5a7
Gabriel Schneider <gabrielmunizsch@gmail.com>
2022-07-13 17:56:42 -0300

    refatore: payment service

--- src/service/payment.service.ts
+++ src/service/payment.service.ts
@@ -1,29 +1,35 @@
-import { AppDataSource } from "../data-source"
-import { User } from "../entities/user.entity"
-import { AppError } from "../errors/AppError"
+import { AppDataSource } from "../data-source";
+import { User } from "../entities/user.entity";
+import { AppError } from "../errors/AppError";
 
+export class PaymentService {
+  static async createPaymentService(
+    user_id: string,
+    email: string,
+    description: string,
+    amount: string
+  ) {
+    const userRepository = AppDataSource.getRepository(User);
 
-export const paymentService = async(user_id:string,email:string,description:string,amount:string) => {
-    const userRepository = AppDataSource.getRepository(User)
+    const user = await userRepository.findOne({ where: { id: user_id } });
 
-    const user = await userRepository.findOne({where : {id: user_id}})
-
-    if(!user){
-        throw new AppError(404, "user notfound")
+    if (!user) {
+      throw new AppError(404, "user notfound");
     }
-    if(!user_id || !email || !description || !amount){
-        throw new AppError(404, "Error in your request")
+
+    if (!user_id || !email || !description || !amount) {
+      throw new AppError(404, "Error in your request");
     }
 
     const item = {
-        orderId: user.id,
-        title: description,
-        description: `${description}`,
-        quantity:  1,
-        currency_id: "BRL",
-        unit_price: parseFloat(amount),
-      }
+      orderId: user.id,
+      title: description,
+      description: `${description}`,
+      quantity: 1,
+      currency_id: "BRL",
+      unit_price: parseFloat(amount),
+    };
 
-      return item
-
+    return item;
+  }
 }

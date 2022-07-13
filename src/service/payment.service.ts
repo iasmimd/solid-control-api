c8365cd52e1a8b5391239b5993eb22ac19b9ce58
Gabriel Schneider <gabrielmunizsch@gmail.com>
2022-07-13 17:42:05 -0300

    feat: service payment

---
+++ src/service/payment.service.ts
@@ -0,0 +1,29 @@
+import { AppDataSource } from "../data-source"
+import { User } from "../entities/user.entity"
+import { AppError } from "../errors/AppError"
+
+
+export const paymentService = async(user_id:string,email:string,description:string,amount:string) => {
+    const userRepository = AppDataSource.getRepository(User)
+
+    const user = await userRepository.findOne({where : {id: user_id}})
+
+    if(!user){
+        throw new AppError(404, "user notfound")
+    }
+    if(!user_id || !email || !description || !amount){
+        throw new AppError(404, "Error in your request")
+    }
+
+    const item = {
+        orderId: user.id,
+        title: description,
+        description: `${description}`,
+        quantity:  1,
+        currency_id: "BRL",
+        unit_price: parseFloat(amount),
+      }
+
+      return item
+
+}

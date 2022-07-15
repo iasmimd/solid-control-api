8837396f02b3dd52131ee5ebd936644a271c0f37
iasmim <iasmimstuff@gmail.com>
2022-07-15 15:09:50 -0300

    fix: exclude dist archive

--- dist/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fixedFloat = void 0;
-const fixedFloat = (value) => {
-    return Number.parseFloat((value).toFixed(2));
-};
-exports.fixedFloat = fixedFloat;

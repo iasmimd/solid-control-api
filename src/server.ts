862c7d581e196084557c46751da4f6327f50824c
iasmim <iasmimstuff@gmail.com>
2022-07-15 14:51:53 -0300

    first deploy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import app from "./app";
-import { AppDataSource } from "./data-source";
+import app from './app';
+import { AppDataSource } from './data-source';
 
 (async () => {
   await AppDataSource.initialize().catch((err) => {
-    console.error("Error during Data Source initialization", err);
+    console.error('Error during Data Source initialization', err);
   });
 
-  app.listen(3000, () => console.log("Running at http://localhost:3000"));
+  app.listen(process.env.PORT || 3000, () =>
+    console.log('Running at http://localhost:3000')
+  );
 })();

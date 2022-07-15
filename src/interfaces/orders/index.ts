5cdb62df992964ba2073fb8b01cda882510bed9e
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-07-15 19:40:45 -0300

    feat: created a method that sum total price of orders

--- src/interfaces/orders/index.ts
+++ src/interfaces/orders/index.ts
@@ -1,15 +1,13 @@
 import { Supply } from "../../entities/supply.entity";
 
-interface ISupplies{
-  id: string,
-  qtd: number,
+interface ISupplies {
+  id: string;
+  qtd: number;
 }
 export interface IOrder {
   supplies?: ISupplies[];
   provider_id: string;
-  total_price: number;
   status: string;
-
 }
 
 export interface IOrderUpdate {

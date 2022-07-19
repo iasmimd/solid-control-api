101678b9a52da12d6f84f53fcddbb959b7900b23
Richard Cândido <rich_dacan@protonmail.com>
2022-07-18 22:35:47 -0300

    fix: fixing bugs

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -4,26 +4,15 @@ import {
   PrimaryGeneratedColumn,
   Unique,
   OneToOne,
-  ManyToMany,
   JoinColumn,
   OneToMany,
-  JoinTable,
-<<<<<<< HEAD
-} from "typeorm";
-import { Exclude } from "class-transformer";
-
-import { Cart } from "./cart.entity";
-import { Ticket } from "./ticket.entity";
-=======
   CreateDateColumn,
   UpdateDateColumn,
   DeleteDateColumn,
 } from 'typeorm';
 import { Exclude } from 'class-transformer';
-import { Address } from './address.entity';
 import { Cart } from './cart.entity';
 import { Ticket } from './ticket.entity';
->>>>>>> refactor/adminLogic
 
 @Entity()
 @Unique(["email"])
@@ -48,7 +37,6 @@ class User {
   @Exclude()
   active: boolean;
 
-<<<<<<< HEAD
   @Column()
   number: string;
 
@@ -57,26 +45,21 @@ class User {
 
   @Column( { length: 256 })
   complement: string;
-=======
   @CreateDateColumn()
   created_at: Date;
 
   @UpdateDateColumn()
   updated_at: Date;
->>>>>>> refactor/adminLogic
 
   @Column( { length: 2, nullable: false })
   state: string;
 
-<<<<<<< HEAD
   @Column()
   zip_code: string;
 
   @Column( { length: 256, nullable: false })
   country: string;
 
-=======
->>>>>>> refactor/adminLogic
   @OneToOne(() => Cart, { eager: true })
   @JoinColumn()
   cart: Cart;

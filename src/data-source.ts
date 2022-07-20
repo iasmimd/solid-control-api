18aec7c45904d18431a0d52fe5acd6ed2b4874a3
iasmim <iasmimstuff@gmail.com>
2022-07-20 20:52:42 -0300

    update data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -21,11 +21,11 @@ export const AppDataSource =
             logging: false,
             entities:
               process.env.NODE_ENV === 'production'
-                ? ['dist/entities/*.js']
+                ? ['dist/src/entities/*.js']
                 : ['src/entities/*.ts'],
             migrations:
               process.env.NODE_ENV === 'production'
-                ? ['dist/migrations/*.js']
+                ? ['dist/src/migrations/*.js']
                 : ['src/migrations/*.ts'],
           });
   

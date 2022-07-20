7d9db243baf29e2032752728492170ae415f9af5
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-07-20 17:16:17 -0300

    feat: added update status route

--- src/routers/tickets.routes.ts
+++ src/routers/tickets.routes.ts
@@ -1,12 +1,13 @@
-import { Router } from 'express';
-import TicketController from '../controller/ticket.controller';
-import { authUser } from '../middleware/authToken.middleware';
+import { Router } from "express";
+import TicketController from "../controller/ticket.controller";
+import { authUser } from "../middleware/authToken.middleware";
 
 const routes = Router();
 
 export const ticketRoutes = () => {
-  routes.post('', authUser, TicketController.create)
-  routes.get('', TicketController.read)
-  
-  return routes
+  routes.post("", authUser, TicketController.create);
+  routes.get("", authUser, TicketController.read);
+  routes.patch("/:ticket_id", authUser, TicketController.update);
+
+  return routes;
 };

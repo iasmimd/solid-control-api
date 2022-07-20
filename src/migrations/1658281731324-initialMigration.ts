93047d54fd086a1f8295e7a78d901b626ad40019
Gabriel Schneider <gabrielmunizsch@gmail.com>
2022-07-19 23:06:01 -0300

    feat: orders tests

--- src/migrations/1658281731324-initialMigration.ts
+++ src/migrations/1658281731324-initialMigration.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initialMigration1658260832489 implements MigrationInterface {
+export class initialMigration1658281731324 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }

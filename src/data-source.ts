9c97984fd8f12a0b405c3d747fe49158725b1579
iasmim <iasmimstuff@gmail.com>
2022-07-20 20:46:22 -0300

    update deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,18 +10,24 @@ export const AppDataSource =
         entities: ["src/entities/*.ts"],
         synchronize: true,
       })
-    : new DataSource({
-        type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        synchronize: false,
-        logging: true,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      });
+      : new DataSource({
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            ssl:
+              process.env.NODE_ENV === 'production'
+                ? { rejectUnauthorized: false }
+                : false,
+            synchronize: false,
+            logging: false,
+            entities:
+              process.env.NODE_ENV === 'production'
+                ? ['dist/entities/*.js']
+                : ['src/entities/*.ts'],
+            migrations:
+              process.env.NODE_ENV === 'production'
+                ? ['dist/migrations/*.js']
+                : ['src/migrations/*.ts'],
+          });
   
 
 
